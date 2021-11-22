da8490c4cf8c977d396825c3e32853091c87d46d
Bohdan Hawrylyshyn <d3cembry@gmail.com>
2021-11-22 17:22:16 +0300

    add weather icons and backgrounds

--- iOSWeatherApp/iOSWeatherApp/Controllers/WeatherController.swift
+++ iOSWeatherApp/iOSWeatherApp/Controllers/WeatherController.swift
@@ -11,32 +11,42 @@ class WeatherController: UIViewController {
     @IBOutlet weak var humidityWindView: UIVisualEffectView!
     @IBOutlet weak var humidityLabel: UILabel!
     @IBOutlet weak var windSpeedLabel: UILabel!
+    @IBOutlet weak var weatherImage: UIImageView!
+    @IBOutlet weak var bgWeatherImage: UIImageView!
     
     var cityName: String = ""
     var lat: Double = 0
     var lon: Double = 0
-    var forecast5Days: [Daily] = []
+    var forecast7Days: [Daily] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         config()
+        loadData(lat: lat, lon: lon)
     }
     
+    //MARK: - config funcs
+    
     private func config() {
-        loadData(lat: lat, lon: lon)
+        configCustomViews()
         configTableView()
     }
     
     private func configTableView() {
-        forecastView.clipsToBounds = true
-        forecastView.layer.cornerRadius = 25
-        humidityWindView.clipsToBounds = true
-        humidityWindView.layer.cornerRadius = 25
         forecast5DayTableView.backgroundColor = .clear
         forecast5DayTableView.delegate = self
         forecast5DayTableView.dataSource = self
         forecast5DayTableView.register(UINib(nibName: "CustomForecastCell", bundle: nil), forCellReuseIdentifier: "CustomForecastCell")
     }
+    
+    private func configCustomViews() {
+        forecastView.clipsToBounds = true
+        forecastView.layer.cornerRadius = 25
+        humidityWindView.clipsToBounds = true
+        humidityWindView.layer.cornerRadius = 25
+    }
+    
+    //MARK: - Load data
     
     private func loadData(lat: Double, lon: Double) {
         loadCurrentWeatherData(lat: lat, lon: lon)
@@ -62,10 +72,40 @@ class WeatherController: UIViewController {
                 DispatchQueue.main.async {
                     self.setCurrentDataToUI(data: jsonData)
                 }
-                forecast5Days = jsonData.daily
+                forecast7Days = jsonData.daily
             } catch {
                 debugPrint("Error while parsing JSON w/ current weather data")
             }
+        }
+    }
+    
+    //MARK: - Set data to UI
+    
+    private func setWeatherImage(weatherDesc: String) {
+        if weatherDesc.contains("clear") {
+            weatherImage.image = UIImage(named: "clear.png")
+            bgWeatherImage.image = UIImage(named: "bgClear.jpg")
+        } else if weatherDesc.contains("clouds") {
+            weatherImage.image = UIImage(named: "cloudy.png")
+            bgWeatherImage.image = UIImage(named: "bgCloudy.jpg")
+        } else if weatherDesc.contains("fog") {
+            weatherImage.image = UIImage(named: "fog.png")
+            bgWeatherImage.image = UIImage(named: "bgFog.jpg")
+        } else if weatherDesc.contains("partly cloudy") {
+            weatherImage.image = UIImage(named: "partlyCloudy.png")
+            bgWeatherImage.image = UIImage(named: "bgPartlyCloudy.jpg")
+        } else if weatherDesc.contains("shower") {
+            weatherImage.image = UIImage(named: "showers.png")
+            bgWeatherImage.image = UIImage(named: "bgShower.jpg")
+        } else if weatherDesc.contains("snow") {
+            weatherImage.image = UIImage(named: "snow.png")
+            bgWeatherImage.image = UIImage(named: "bgSnow.jpg")
+        } else if weatherDesc.contains("thunderstorms") {
+            weatherImage.image = UIImage(named: "thunderstorms.png")
+            bgWeatherImage.image = UIImage(named: "bgThunder.jpg")
+        } else if weatherDesc.contains("windy") {
+            weatherImage.image = UIImage(named: "windy.png")
+            bgWeatherImage.image = UIImage(named: "bgWindy.jpg")
         }
     }
     
@@ -76,6 +116,7 @@ class WeatherController: UIViewController {
         let jsonFeelsLike: String = String(round(data.current.feels_like - 273.15))
         let jsonHumidity: String = String(data.current.humidity)
         let jsonWindSpeed: String = String(data.current.wind_speed)
+        setWeatherImage(weatherDesc: jsonDescription)
         cityNameLabel.text = cityName
         degreesLabel.text = String(round(jsonDegrees - 273.15)) + "°"
         weatherDescriptionLabel.text = jsonDescription
@@ -86,16 +127,17 @@ class WeatherController: UIViewController {
     }
     
 }
+
 extension WeatherController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if forecast5Days.isEmpty {
+        if forecast7Days.isEmpty {
             return 0
         }
-        return 6
+        return forecast7Days.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let daily = forecast5Days[indexPath.row]
+        let daily = forecast7Days[indexPath.row]
         if let cell = tableView.dequeueReusableCell(withIdentifier: "CustomForecastCell", for: indexPath) as? CustomForecastCell {
             cell.backgroundColor = .clear
             let date = Date(timeIntervalSince1970: TimeInterval(daily.dt)).formatted(date: .complete, time: .shortened)
@@ -108,9 +150,5 @@ extension WeatherController: UITableViewDelegate, UITableViewDataSource {
         }
         return UITableViewCell()
     }
-    
-    //    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-    //        return 44
-    //    }
     
 }
